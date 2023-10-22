ea8a958f3e87c142b665e40b8d032e9037bee3ed
arfernandez2000 <arfernandez@itba.edu.ar>
2023-10-22 12:03:06 -0300

    starting landing page

--- harmony-frontend/src/App.tsx
+++ harmony-frontend/src/App.tsx
@@ -1,67 +1,21 @@
 
 // import { Piano } from 'react-nexusui';
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
 import './index.css';
-import { Piano } from './Piano';
-import * as Tone from 'tone'
-import { pitch } from './notes';
-import { socket } from './socket';
-import { useEffect, useState } from 'react';
+import LandingPage from './pages/LandingPage/LandingPage';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
-
-  const [isConnected, setIsConnected] = useState(socket.connected);
-  const [colorId, setColorId] = useState();
-
-  useEffect(() => {
-    function onConnect() {
-      console.log("me conecte al socket desde el cliente!")
-      setIsConnected(true);
-    }
-
-    function onDisconnect() {
-      setIsConnected(false);
-    }
-
-    socket.on('connect', onConnect);
-    socket.on('disconnect', onDisconnect);
-    socket.on('color_id', (value) => {
-      setColorId(value)
-    })
-    socket.on('presskey', (noteToPlay: string) => {
-      console.log(`reproduciendo ${noteToPlay}`)
-      playSynth(noteToPlay)
-    })
-
-    return () => {
-      socket.off('connect', onConnect);
-      socket.off('disconnect', onDisconnect);
-    };
-  }, [socket]);
-
-  // const synth = new Tone.MembraneSynth().toMaster();
-  const synth = new Tone.Synth()
-  synth.oscillator.type = "sine";
-  synth.toMaster();
-
-  function playSynth(note: string) {
-    console.log(`playing ${note}`)
-    synth.triggerAttackRelease(note, "8n")
-  }
-
-  function processPianoClick(change: ({note: number, state: boolean})) {
-    if (change.state) {
-      console.log(change.note)
-      const noteToPlay = pitch[change.note-24]
-      socket.emit('presskey', noteToPlay);
-      // playSynth(noteToPlay)
-    }
-  }
-  
   return (
-    <div>
-      <Piano colorId={colorId}></Piano>
-      {/* <Piano onChange={(change: ({note: number, state: boolean})) => processPianoClick(change)}/> */}
-    </div>
+    <BrowserRouter>
+        <Navbar/>
+      <div className='pt-16'>
+        <Routes>
+          <Route path="/" element={<LandingPage/>}/>
+        </Routes>
+      </div>
+  
+    </BrowserRouter>
   )
 }
 
---
+++ harmony-frontend/src/pages/LandingPage/LandingPage.tsx
@@ -0,0 +1,9 @@
+const LandingPage = () => {
+    return(
+        <div>
+            <h1>Puto el que lee</h1>
+        </div>
+    )
+}
+
+export default LandingPage

d05449069795a5b791a2ed6498bd5660e7129dd8
Azul <azkim@itba.edu.ar>
2024-05-16 21:42:30 -0300

    HAR-69: Added basic mailing service

---
+++ harmony-backend/service/mailService.ts
@@ -0,0 +1,28 @@
+import nodemailer from 'nodemailer';
+import {logger} from "../server";
+
+export const sendMail = async (from: string, to: string, subject: string, html: string) => {
+    const transporter = nodemailer.createTransport({
+        service: process.env.MAIL_HOST,
+        auth: {
+            user: process.env.MAIL_USERNAME,
+            pass: process.env.MAIL_PASSWORD
+        }
+    });
+
+    const mailOptions = {
+        from: from,
+        to: to,
+        subject: subject,
+        html: html
+    };
+
+    logger.info(`Sending mail to - ${to}`);
+    transporter.sendMail(mailOptions, (error, info)=> {
+        if (error) {
+            logger.error(error);
+        } else {
+            logger.info('Email sent: ' + info.response);
+        }
+    });
+}

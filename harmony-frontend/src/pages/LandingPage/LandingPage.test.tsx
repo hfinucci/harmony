975a03b6c4b55ddbf819631f52bf42ca8c3d6e07
Azul <azkim@itba.edu.ar>
2024-05-27 14:52:20 -0300

    Fixed one test and commented other

--- harmony-frontend/src/pages/LandingPage/LandingPage.test.tsx
+++ harmony-frontend/src/pages/LandingPage/LandingPage.test.tsx
@@ -1,10 +1,15 @@
 import { render, screen } from "@testing-library/react";
 import LandingPage from "./LandingPage";
 import { describe, expect, test } from "vitest";
+import {BrowserRouter} from "react-router-dom";
 
 describe("Test LandingPage", () => {
     test("renders correctly", () => {
-        render(<LandingPage />);
+        render(
+            <BrowserRouter>
+                <LandingPage />
+            </BrowserRouter>
+        );
 
         expect(screen.getByText("Harmony")).toBeInTheDocument();
         expect(

ac3b6c6fe0741e327c2dafdb512dbbcf0a45e0a6
Azul Candelaria Kim <58495153+AzuCande@users.noreply.github.com>
2024-06-16 20:05:36 -0300

    Added email templates (#34)

--- harmony-backend/service/mailService.ts
+++ harmony-backend/service/mailService.ts
@@ -1,25 +1,40 @@
 import nodemailer from 'nodemailer';
 import {logger} from "../server";
+import * as fs from "node:fs";
+import * as HandleBars from "handlebars";
 
 const FROM = "no-reply@harmony-itba.com"
 
 export class MailService {
     public static async sendJoinOrgRequestMail(user: string, org: number) {
         const subject = "You've been invited to join an organization!";
-        const html = `<p>Hi there!</p>
-        <p>You've been invited to join an organization on Harmony!</p>
-        <p>Click <a href="http://localhost:5173/accept-invitation?user=${user}&org=${org}">here</a> to join the organization!</p>
-        <p>Thanks for using Harmony!</p>`;
+
+        const emailTemplateSource = fs.readFileSync('/app/service/templates/emailTemplate.hbs', 'utf8');
+        const template = HandleBars.compile(emailTemplateSource);
+
+        const joinUrl = `http://localhost:5173/accept-invitation?user=${user}&org=${org}`;
+        const html = template({
+            message: "You've been invited to join an organization on Harmony!",
+            clickme: "CLICK HERE TO JOIN",
+            url: joinUrl
+        });
 
         await this.sendMail(user, subject, html);
     }
 
     public static async sendNewMemberJoinedMail(user: string[], org: number) {
         const subject = "A new member has joined your organization!";
-        const html = `<p>Hi there!</p>
-        <p>A new member has joined your organization on Harmony!</p>
-        <p>Click <a href="http://localhost:5173/org/${org}">here</a> to see who joined!</p>
-        <p>Thanks for using Harmony!</p>`;
+
+        const emailTemplateSource = fs.readFileSync('/app/service/templates/emailTemplate.hbs', 'utf8');
+        const template = HandleBars.compile(emailTemplateSource);
+
+        const viewUrl = `http://localhost:5173/orgs/${org}`;
+
+        const html = template({
+            message: "A new member has joined your organization on Harmony!",
+            clickme: "CLICK HERE TO SEE WHO JOINED",
+            url: viewUrl
+        });
 
         await this.sendMail(user, subject, html);
     }

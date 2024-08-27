71dbfc8d0e4773d6fd43989ce33dd65affbc0a78
arfernandez2000 <arfernandez@itba.edu.ar>
2024-08-27 11:38:49 +0200

    Fixed date format to be more appealing

--- harmony-frontend/src/components/SongCard/SongCard.tsx
+++ harmony-frontend/src/components/SongCard/SongCard.tsx
@@ -3,6 +3,7 @@ import { FaEdit } from "react-icons/fa";
 import {SongService} from "../../service/songService.ts";
 import { useNavigate } from "react-router-dom";
 import DeleteSongModal from "../DeleteSongModal/DeleteSongModal";
+import { formatDate } from "../../utils"
 
 interface SongCardProps {
     song: Song;
@@ -21,8 +22,8 @@ const SongCard = (
         <tr  className={song.org? "grid grid-cols-5 w-full" : "grid grid-cols-4 w-full"}>
             <td className={"text-gray-500"}>{song.name}</td>
             {song.org && <td className={"text-gray-500"}>{song.org}</td>}
-            <td className={"text-gray-500"}>{song.created}</td>
-            <td className={"text-gray-500"}>{song.lastmodified}</td>
+            <td className={"text-gray-500"}>{formatDate(song.created)}</td>
+            <td className={"text-gray-500"}>{formatDate(song.lastmodified)}</td>
             <td className="text-3xl text-fuchsia-950 flex flex-row">
                 <button onClick={() => nav(`/songs/${song.id}`)} className={"hover:text-fuchsia-700 flex justify-center mr-8"}>
                     <FaEdit />
